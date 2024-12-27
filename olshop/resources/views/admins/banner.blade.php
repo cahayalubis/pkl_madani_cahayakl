02536d00f42e727950b2d1746545e1134754d521
cahayalubis <cahayakamila2040@gmail.com>
2024-12-27 14:33:07 +0700

    first commit

---
+++ olshop/resources/views/admins/banner.blade.php
@@ -0,0 +1,93 @@
+<x-layout-component>
+
+ <!-- Table Start -->
+ <div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-12">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Tombol Tambah Data Banner</h6>
+                <div class="m-n2">
+                    <a href="{{route('addbanner')}}">
+                    <button type="button" class="btn btn-primary m-2">Tombol Tambah Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Banner Table</h6>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Judul</th>
+                                <th scope="col">Deskripsi</th>
+                                <th scope="col">Gambar</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>jhon@email.com</td>
+                                <td>USA</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-sm-12 col-xl-6">
+    <div class="bg-light rounded h-100 p-4">
+        <h6 class="mb-4">Basic Buttons</h6>
+        <div class="m-n2">
+            <button type="button" class="btn btn-primary m-2">Primary</button>
+            <button type="button" class="btn btn-secondary m-2">Secondary</button>
+            <button type="button" class="btn btn-success m-2">Success</button>
+            <button type="button" class="btn btn-danger m-2">Danger</button>
+            <button type="button" class="btn btn-warning m-2">Warning</button>
+            <button type="button" class="btn btn-info m-2">Info</button>
+            <button type="button" class="btn btn-light m-2">Light</button>
+            <button type="button" class="btn btn-dark m-2">Dark</button>
+            <button type="button" class="btn btn-link m-2">Link</button>
+        </div>
+    </div>
+</div>
+<div class="col-sm-12 col-xl-6">
+    <div class="bg-light rounded h-100 p-4">
+        <h6 class="mb-4">Basic Buttons</h6>
+        <div class="m-n2">
+            <button type="button" class="btn btn-primary m-2">Primary</button>
+            <button type="button" class="btn btn-secondary m-2">Secondary</button>
+            <button type="button" class="btn btn-success m-2">Success</button>
+            <button type="button" class="btn btn-danger m-2">Danger</button>
+            <button type="button" class="btn btn-warning m-2">Warning</button>
+            <button type="button" class="btn btn-info m-2">Info</button>
+            <button type="button" class="btn btn-light m-2">Light</button>
+            <button type="button" class="btn btn-dark m-2">Dark</button>
+            <button type="button" class="btn btn-link m-2">Link</button>
+        </div>
+    </div>
+</div>
+<div class="col-sm-12 col-xl-6">
+    <div class="bg-light rounded h-100 p-4">
+        <h6 class="mb-4">Basic Buttons</h6>
+        <div class="m-n2">
+            <button type="button" class="btn btn-primary m-2">Primary</button>
+            <button type="button" class="btn btn-secondary m-2">Secondary</button>
+            <button type="button" class="btn btn-success m-2">Success</button>
+            <button type="button" class="btn btn-danger m-2">Danger</button>
+            <button type="button" class="btn btn-warning m-2">Warning</button>
+            <button type="button" class="btn btn-info m-2">Info</button>
+            <button type="button" class="btn btn-light m-2">Light</button>
+            <button type="button" class="btn btn-dark m-2">Dark</button>
+            <button type="button" class="btn btn-link m-2">Link</button>
+        </div>
+    </div>
+</div>
+<!-- Table End -->
+</x-layout-component>
